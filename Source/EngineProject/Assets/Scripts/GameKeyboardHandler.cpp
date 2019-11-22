97952b382b7e24c8d175aac4b17316c404b06830
rshimoda <rshimoda@gmail.com>
2019-11-22 16:36:33 -0500

    Finishing testing the handlers

---
+++ Source/EngineProject/Assets/Scripts/GameKeyboardHandler.cpp
@@ -0,0 +1,10 @@
+#include "GameKeyboardHandler.h"
+
+namespace Game {
+	bool GameKeyboardHandler::VOnKeyDown(unsigned int const kcode) {
+		return false;
+	}
+	bool GameKeyboardHandler::VOnKeyUp(unsigned int const kcode) {
+		return false;
+	}
+}
