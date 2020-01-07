b345465a5a46db86ca370293a67dcb77cd02586a
Ekin Karayalçın <ekokara@gmail.com>
2020-01-07 11:45:47 -0500

    SceneGraph 1st Implementation
    Just copied the code from Week1 and fixed the syntax errors.

--- Source/EngineProject/Base/Actor.cpp
+++ Source/EngineProject/Base/Actor.cpp
@@ -4,6 +4,15 @@
 Actor::Actor()
 {
 	CoCreateGuid(&id);
+	parent = NULL;
+}
+
+Actor::~Actor(void)
+{
+	for (unsigned int i = 0; i < children.size(); i++)
+	{
+		delete children[i];
+	}
 }
 
 void Actor::AddComponent(ActorComponent* _component)
@@ -22,4 +31,26 @@ ActorComponent* Actor::GetComponent(string componentName)
 		}
 	}
 	return NULL;
+}
+
+void Actor::AddChild(Actor* s) 
+{
+	children.push_back(s);
+	s->parent = this;
+}
+
+void Actor::Update(float msec) 
+{
+	if (parent) 
+	{ //This node has a parent...
+		worldTransform = parent->worldTransform * localTransform;
+	}
+	else 
+	{ //Root node, world transform is local transform!
+		worldTransform = localTransform;
+	}
+	for (vector<Actor*>::iterator i = children.begin(); i != children.end(); ++i)
+	{
+		(*i)->Update(msec);
+	}
 }
