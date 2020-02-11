f41434de3856fea02799b9cf2f2820037412ab9d
Broddiccus <36047634+Broddiccus@users.noreply.github.com>
2020-02-11 11:09:02 -0500

    updated Audio Component
    Audio Component is implimented but untested, working on the testing now

--- Source/EngineProject/Components/AudioComponent/AudioComponent.cpp
+++ Source/EngineProject/Components/AudioComponent/AudioComponent.cpp
@@ -1,5 +1,91 @@
+#include <iostream>
 #include "AudioComponent.h"
 
-namespace Component {
+AudioComponent::AudioComponent(std::string fN, bool l, float p, float v)
+{
+		fileName = fN;
+		looping = l;
+		music.setLoop(looping);
+		pitch = p;
+		music.setPitch(pitch);
+		volume = v;
+		music.setVolume(volume);
+}
 
+bool AudioComponent::Play()
+{
+
+	if (!music.openFromFile(fileName)) { //checking if the file is there
+		cout << "File Not Found!";
+		return false;
+	}
+	
+	music.play();
+	return true;
+}
+
+bool AudioComponent::Stop()
+{
+	music.stop();
+	return true;
+}
+
+bool AudioComponent::Pause()
+{
+	music.pause();
+	return true;
+}
+
+sf::SoundSource::Status AudioComponent::GetState()
+{
+	return music.getStatus();
+}
+
+float AudioComponent::GetPitch()
+{
+	return pitch;
+}
+
+void AudioComponent::SetPitch(float f)
+{
+	pitch = f;
+	music.setPitch(pitch);
+}
+
+float AudioComponent::GetVolume()
+{
+	return volume;
+}
+
+void AudioComponent::SetVolume(float f)
+{
+	volume = f;
+	music.setVolume(volume);
+}
+
+bool AudioComponent::IsLooping()
+{
+	return looping;
+}
+
+void AudioComponent::SetLooping(bool b)
+{
+	looping = b;
+	music.setLoop(looping);
+}
+
+bool AudioComponent::SetFile(std::string s)
+{
+	music.stop(); //not sure how this will interact when playing, so i'm hard killing the song before swapping files
+	fileName = s;
+	if (!music.openFromFile(fileName)) {
+		cout << "File Not Found!";
+		return false;
+	}
+	return true;
+}
+
+std::string AudioComponent::GetFile()
+{
+	return fileName;
 }
--- Source/EngineProject/Components/AudioComponent/AudioComponent.h
+++ Source/EngineProject/Components/AudioComponent/AudioComponent.h
@@ -1,11 +1,31 @@
 #ifndef __COMPONENT_AUDIO_COMPONENT_H__
 #define __COMPONENT_AUDIO_COMPONENT_H__
+#include <SFML/Audio.hpp>
+#include "../../Base/ActorComponent.h" 
 
-#include "IAudioComponent.h" 
 
-namespace Component {
-	class AudioComponent : public IAudioComponent{
-	};
-}
+class AudioComponent : public ActorComponent{
+protected:
+	bool looping;
+	std::string fileName;
+	float pitch;
+	float volume;
+	sf::Music music;
+public:
+	AudioComponent(std::string fN = std::string(""), bool l = false, float p = 1.0f, float v = 1.0f);
+	bool Play();
+	bool Stop();
+	bool Pause();
+	sf::SoundSource::Status GetState();
+	float GetPitch();
+	void SetPitch(float f);
+	float GetVolume();
+	void SetVolume(float f);
+	bool IsLooping();
+	void SetLooping(bool b);
+	bool SetFile(std::string s);
+	std::string GetFile();
+};
+
 
 #endif // __COMPONENT_AUDIO_COMPONENT_H__
